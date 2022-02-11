c4917b7d2ee0a374328ad343b656322d029f0b3a
DAM1ALU12 <DAM1ALU12@B01-PC12.IESGUARA.LOCAL>
2022-02-11 14:19:34 +0100

    .

---
+++ Aprende_Java_con_Ejercicios/src/Ficheros_pags_171_172/Ejercicio_03.java
@@ -0,0 +1,14 @@
+package Ficheros_pags_171_172;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+
+public class Ejercicio_03 {
+
+	public static void main(String[] args) {
+		String a1="archivos/aaaaa.dat";
+		
+
+	}
+
+}
