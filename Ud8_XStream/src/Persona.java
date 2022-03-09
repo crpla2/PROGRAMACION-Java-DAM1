118b34ed17dfb1cd7b56207cf8076736a1fae0d6
DAM1ALU12 <DAM1ALU12@B01-PC12.IESGUARA.LOCAL>
2022-03-09 13:16:32 +0100

    uds8

---
+++ Ud8_XStream/src/Persona.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+public class Persona implements Serializable {
+	private String nombre;
+	private int edad;
+	public Persona(String nombre, int edad) {
+		this.nombre=nombre;
+		this.edad=edad;
+		
+	}
+	public Persona(){
+		this.nombre=null;
+	}
+	public void setNombre (String nom) { nombre=nom;}
+	public void setEdad (int ed) {edad=ed;}
+	
+	public String getNombre() {return nombre;}
+	public int getEdad() {return edad;}
+
+}
