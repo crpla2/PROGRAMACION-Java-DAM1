ff199f1261800bd8feb2a5867d76041aeed29d0e
DAM1ALU12 <DAM1ALU12@B01-PC12.IESGUARA.LOCAL>
2022-03-21 12:24:16 +0100

    creado main

--- Ud10_JDBCSesion1/src/paquete1/HolaMundoGestionErrores.java
+++ Ud10_JDBCSesion1/src/paquete1/HolaMundoGestionErrores.java
@@ -9,10 +9,10 @@ public class HolaMundoGestionErrores {
 	    private static String driver = "com.mysql.cj.jdbc.Driver";
 	    private static String database = "sample";
 	    private static String hostname = "localhost";
-	    private static String port = "3306";
+	    private static String port = "3308";
 	    private static String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?serverTimezone=Europe/Madrid";
 	    private static String username = "root";
-	    private static String password = "rootroot";
+	    private static String password = "root";
 	public static void main(String[] args) {
      
      try {
