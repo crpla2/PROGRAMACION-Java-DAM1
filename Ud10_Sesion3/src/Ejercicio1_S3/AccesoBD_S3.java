469ffce8e1f31c4f4ae9e2ab245badc36983c152
DAM1ALU12 <DAM1ALU12@B01-PC12.IESGUARA.LOCAL>
2022-04-06 13:21:22 +0200

    sesion3 terminada

--- Ud10_Sesion3/src/Ejercicio1_S3/AccesoBD_S3.java
+++ Ud10_Sesion3/src/Ejercicio1_S3/AccesoBD_S3.java
@@ -48,53 +48,121 @@ public class AccesoBD_S3 {
 			e.setSalarioL(reg.getDouble(6));
 			e.setComision(reg.getDouble(7));
 			e.setDepartamento(reg.getInt(8));
+			consulta.close();
 			return e;
+			
 		} else
 			return null;
+	}
+
+	public ArrayList<Empleado> busquedaPorPuesto(String puesto)   {
+		ArrayList<Empleado>lista=new ArrayList<Empleado>();
+		try {
+			cadenaSQL= "select * from emp where JOB = ? ";
+			consulta = conecta.prepareStatement(cadenaSQL);
+			consulta.setString(1, puesto);
+			ResultSet reg = consulta.executeQuery();
+
+			while (reg.next()) {
+				lista.add(new Empleado(
+						reg.getInt(1), 
+						reg.getString(2), 
+						reg.getString(3), 
+						reg.getInt(4), 
+						reg.getDate(5),
+						reg.getDouble(6), 
+						reg.getDouble(7), 
+						reg.getInt(8)));
+			}
+			consulta.close();
+		} catch (SQLException e) {
+			lista=null;
+		}
+		
+		return lista;
 
 	}
 
-	public ArrayList<Empleado> busquedaPorPuesto(String puesto) throws SQLException {
+	public ArrayList<Empleado> consultaPorAntiguedad(java.sql.Date fecha) {
 		ArrayList<Empleado>lista=new ArrayList<Empleado>();
-		cadenaSQL= "select * from emp where JOB = ? ";
-		consulta = conecta.prepareStatement(cadenaSQL);
-		consulta.setString(1, puesto);
-		ResultSet reg = consulta.executeQuery();
-
-		while (reg.next()) {
-			lista.add(new Empleado(
-					reg.getInt(1), 
-					reg.getString(2), 
-					reg.getString(3), 
-					reg.getInt(4), 
+		try {
+			String cadenaSQL="select * from emp where HIREDATE <= ?";
+			PreparedStatement consulta= conecta.prepareStatement(cadenaSQL);
+			consulta.setDate(1, fecha);
+			ResultSet reg=consulta.executeQuery();
+			while(reg.next()) {
+				lista.add(new Empleado(
+					reg.getInt(1),
+					reg.getString(2),
+					reg.getString(3),
+					reg.getInt(4),
 					reg.getDate(5),
-					reg.getDouble(6), 
-					reg.getDouble(7), 
+					reg.getDouble(6),
+					reg.getDouble(7),
 					reg.getInt(8)));
+				
+			}
+			consulta.close();
+		} catch (SQLException e) {
+			lista=null;
 		}
-		return lista;
-
-	}
-
-	public ArrayList<Empleado> consultaPorAntiguedad(java.sql.Date fecha) throws SQLException{
-		ArrayList<Empleado>lista=new ArrayList<Empleado>();
-		String cadenaSQL="select * from emp where HIREDATE <= ?";
-		PreparedStatement consulta= conecta.prepareStatement(cadenaSQL);
-		consulta.setDate(1, fecha);
-		ResultSet reg=consulta.executeQuery();
-		while(reg.next()) {
-			lista.add(new Empleado(
-				reg.getInt(1),
-				reg.getString(2),
-				reg.getString(3),
-				reg.getInt(4),
-				reg.getDate(5),
-				reg.getDouble(6),
-				reg.getDouble(7),
-				reg.getInt(8)));
-		}
-		
 		return lista;
 		
 	}
+	public int insertarConBean(Empleado emp) {
+		String cadenaSQL="insert into emp values(?,?,?,?,?,?,?,?)";
+		try {
+			PreparedStatement consulta= conecta.prepareStatement(cadenaSQL);
+			consulta.setInt(1, emp.getNumero());
+			consulta.setString(2, emp.getNombre());
+			consulta.setString(3, emp.getPuesto());
+			consulta.setInt(4, emp.getJefe());
+			consulta.setDate(5,emp.getAntiguedad());
+			consulta.setDouble(6, emp.getSalario());
+			consulta.setDouble(7, emp.getComision());
+			consulta.setInt(8, emp.getDepartamento());
+			
+			consulta.executeUpdate();
+			consulta.close();
+			return 1;
+		} catch (SQLException e) {
+			System.out.print(" ERROR: ");
+			return e.getErrorCode();
+			
+		}		
+	}
+	public int actualizarSalario(int departamento, double porcentaje) {
+		String cadenaSQL="update emp set SAL=SAL+(SAL*?) where DEPTNO=?";
+		try {
+			PreparedStatement consulta= conecta.prepareStatement(cadenaSQL);
+			consulta.setDouble(1, porcentaje);
+			consulta.setInt(2, departamento);
+			
+			return consulta.executeUpdate();
+			
+		}catch (SQLException e){
+			System.out.print(" ERROR: ");
+			return e.getErrorCode();
+		}
+		
+		
+	}
+	 public int borrarEmpleado (int numero) {
+		 String cadenaSQL="delete from emp  where EMPNO=?";
+			try {
+				PreparedStatement consulta= conecta.prepareStatement(cadenaSQL);
+				consulta.setInt(1, numero);
+				
+				
+				return consulta.executeUpdate();
+				
+			}catch (SQLException e){
+				return -1;
+			}
+			
+		 
+		
+		 
+	 }
+	//insert delete update consulta.executeUpdate()
 }
--- Ud10_Sesion3/src/Ejercicio1_S3/Main.java
+++ Ud10_Sesion3/src/Ejercicio1_S3/Main.java
@@ -10,14 +10,44 @@ public class Main {
 	static AccesoBD_S3 abd= new AccesoBD_S3();
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		abd.conectar();
+		//C
 //		System.out.println(abd.busquedaPorCodigo(100));
 //		System.out.println(abd.busquedaPorCodigo(7788));
+		//D
 //		System.out.println(abd.busquedaPorPuesto("Profesor"));
 //		System.out.println(abd.busquedaPorPuesto("CLERK"));
-		
-		System.out.println("Empleados contratados antes del 22 de febrero de 1981");
-		System.out.println("=====================================================");
+		//E
+//		System.out.println("Empleados contratados antes del 22 de febrero de 1981");
+//		System.out.println("=====================================================");
 		System.out.println(abd.consultaPorAntiguedad(dateToSqlDate.fechaSQL("1981/02/22")));
+		//F
+//		Empleado e1 = new Empleado(1, "CARRERA", "PROFESOR", 7788, dateToSqlDate.fechaSQL("2020/03/18"), 700, 0, 20);
+//		System.out.println(abd.insertarConBean(e1)); // 1 (y se inserta en la tabla)
+//		//Repito la misma instrucción:
+//		System.out.println(abd.insertarConBean(e1)); 
+//		// 1062, ya existe un registro con esa clave!!!!
+		//G
+//		Empleado e2 = new Empleado(10, "CARRERA", "PROFESOR", 7788, dateToSqlDate.fechaSQL("2020/03/18"), 700, 0, 35);
+//		System.out.println(abd.insertarConBean(e2)); 
+//		//Ya existe un registro con esa clave, ERROR: 1452 El numero de departamento no existe, da error por la clave foranea
+//		//H
+//		Empleado e3 = new Empleado(10, "CARRERA", "PROFESOR",null, dateToSqlDate.fechaSQL("2020/03/18"),null, 35);
+//		System.out.println(abd.insertarConBean(e3)); 
+		//I
+		System.out.println("Subir un 20% el salario de los empleados del Departamento 30");
+		System.out.println(abd.actualizarSalario(30, 0.2)); //6
+//		//
+		System.out.println("Subir un 15% el salario de los empleados del Departamento 44");
+		System.out.println(abd.actualizarSalario(44, 0.15));//0
+//		// No existe el departamento 44
+		//J
+//		System.out.println(abd.borrarEmpleado(1));
+		//1 - Se ha borrado el empleado de código 1 de la tabla
+//		System.out.println(abd.borrarEmpleado(99));
+		//0 – No se ha borrado ninguno pues no existe el empleado 99
+//		System.out.println(abd.borrarEmpleado(7839));
+		//1 – Se ha borrado el empleado 7839.
+		
 		abd.desconectar();
 	}
 
