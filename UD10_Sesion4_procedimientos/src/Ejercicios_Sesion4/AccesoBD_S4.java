41b3b61cd7111cac192cb4816242f58b1f968adf
DAM1ALU12 <DAM1ALU12@B01-PC12.IESGUARA.LOCAL>
2022-04-08 13:01:44 +0200

    Terminado ejercicio 3 sesion4

--- UD10_Sesion4_procedimientos/src/Ejercicios_Sesion4/AccesoBD_S4.java
+++ UD10_Sesion4_procedimientos/src/Ejercicios_Sesion4/AccesoBD_S4.java
@@ -57,9 +57,9 @@ public class AccesoBD_S4 {
 			if (rs.next()) {
 				System.out.println("Nombre Producto - Precio - Exsistencias - Mínimo");
 				System.out.println("--------------------------------------------------");
-				System.out.println(rs.getString(1) + " | " + rs.getBigDecimal(2) + " | " + rs.getInt(3) + " | " + rs.getInt(4));
+				System.out.println(rs.getString(1) + " | " + rs.getDouble(2) + " | " + rs.getInt(3) + " | " + rs.getInt(4));
 				while (rs.next()) {
-					System.out.println(rs.getString(1) +" | " + rs.getBigDecimal(2) + " | "+ rs.getInt(3) + " | " + rs.getInt(4));
+					System.out.println(rs.getString(1) +" | " + rs.getDouble(2) + " | "+ rs.getInt(3) + " | " + rs.getInt(4));
 				} 
 			}else System.out.println("La categoria "+categoria+" no existe o no tiene productos bajo mínimos");
 		      
@@ -71,5 +71,26 @@ public class AccesoBD_S4 {
 		}
 	      
 		 
+	}
+	
+	public void  incrementarPC(int categoria, int cantidad) {
+		try {
+			int salida = 0;
+			CallableStatement proc = conecta.prepareCall("CALL incrementarPrecioCategoria (?,?,?) ");
+			proc.setInt(1, categoria);
+			proc.setInt(2, cantidad);
+			proc.registerOutParameter(3,Types.INTEGER);
+			proc.execute(); 
+			if (proc.getInt(3)>=0) {
+				if(proc.getInt(3)==0) System.out.println("No se actualizó ningún producto para la categoría " + categoria);
+				else System.out.println("Se incrementó el precio en un "+ cantidad +" para los productos de la categoría "+categoria);
+			}
+			else System.out.println("Ocurrió un ERROR");
+				
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
